a77574eedc4303c0b5d14c40a972642b47900d0c
Greismorr <Greismorr@users.noreply.github.com>
2023-12-12 14:15:07 +0000

    deploy: 9c8df06689c6bf516fd536ad795b26191a90b575

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"manifest.json": "6587237943913f73f1453fd81aa19fd8",
+const RESOURCES = {"manifest.json": "126d6bc395925fd9e1e5fb395e2330a8",
 "favicon.png": "935197c6a559b5b46908fd04b1303f61",
-"index.html": "c7d8254388aa3680bd597744363b3cb1",
-"/": "c7d8254388aa3680bd597744363b3cb1",
+"index.html": "93de3d285a24fac65b51012677fe6a03",
+"/": "93de3d285a24fac65b51012677fe6a03",
 "main.dart.js": "aac365e49767d5ee15f7bb98a3a1078a",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.json": "bc431bf61caa0dbdc397b4999c3dd414",
